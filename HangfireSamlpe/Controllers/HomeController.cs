4f357011cfc11ee375a08b28c7621d6f271853b9
d-kezuka <d-kezuka@e-reverse.com>
2018-10-04 17:02:17 +0900

    New !

---
+++ HangfireSamlpe/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hangfire;
+
+
+namespace HangfireSamlpe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Hangfireでjobをキューに登録
+            var id = BackgroundJob.Enqueue(() => Console.WriteLine("Simple Job"));
+            BackgroundJob.ContinueWith(id, () => Console.WriteLine("world!"));
+            return View();
+        }
+    }
+}
---
+++ HangfireSamlpe/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using Hangfire;
+using Hangfire.MySql;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HangfireSamlpe.Startup))]
+
+namespace HangfireSamlpe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            // 接続文字列は環境に応じて変えること
+            var constr =
+                @"Server=localhost;Port=3306;Uid=root;Pwd=P@ssw0rd;database=hangfireDB;DefaultCommandTimeout=9999;Charset=utf8;Allow User Variables=True";
+            GlobalConfiguration.Configuration.UseStorage(
+                new MySqlStorage(
+                    constr,
+                    new MySqlStorageOptions
+                    {
+                        // !cannot use
+                        // TransactionIsolationLevel = IsolationLevel.ReadCommitted,
+                        QueuePollInterval = TimeSpan.FromSeconds(15),
+                        JobExpirationCheckInterval = TimeSpan.FromHours(1),
+                        CountersAggregateInterval = TimeSpan.FromMinutes(5),
+                        /* データベースによっては 'Error Code: 1071. Specified key was too long; max key length is 767 bytes'が発生
+                         ->Table `SET`, `Value` nvarchar(256) NOT NULL*/
+                        // PrepareSchemaIfNecessary = true,
+                        DashboardJobListLimit = 50000,
+                        TransactionTimeout = TimeSpan.FromMinutes(1),
+                        // !cannot use
+                        // TablesPrefix = "Hangfire"
+                    }));
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+        }
+    }
+}
